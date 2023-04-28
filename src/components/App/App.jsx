1b3889a80404c508a2f993f63ad4aaf558461973
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-28 18:41:33 -0400

    ListItem currently produces bare minimum for now

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/Header'
+import ListItem from '../GalleryItem/GalleryItem';
 
 function App() {
     return (
       <div className="App">
        <Header />
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <ListItem />
       </div>
     );
 }
---
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,12 @@
+function ListItem () {
+    return (
+        <>
+         <img src="images/goat_small.jpg"/> 
+         <br></br> 
+         <button>Love it!</button>
+         <p>0 people like this 😞</p> 
+        </>
+    )
+}
+
+export default ListItem;
