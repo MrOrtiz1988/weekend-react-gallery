2fdb18b917162dbced16f57083a4a6293c12dad2
Luis Ortiz <l.ortizintec@gmail.com>
2023-04-29 19:56:42 -0400

    form component now works, woohoo!

---
+++ src/components/GalleryForm/GalleryForm.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import axios from "axios";
+
+function GalleryForm (props) {
+
+    const sendData = (event) => {
+        event.preventDefault();
+        axios({
+          method: 'POST',
+          url: '/gallery',
+          data: {
+            path: props.imagePath,
+            description: props.description
+          }
+        }).then((response) => {
+          props.fetchGallery();
+          props.setImagePath('');
+          props.setDescription('');
+        }).catch((error) => {
+          console.log('whoopsie on sendData', error);
+        })
+      }
+
+    return (
+        <form onSubmit={sendData}>
+            <h2>Add a Photo!</h2>
+        <div> 
+            <label>Path: <input
+                type='text'
+                placeholder="Where is the image?"
+                value={props.imagePath}
+                onChange={(event) => {props.setImagePath(event.target.value)}}
+            /></label>
+            
+            <label>Description: <input 
+                type='text'
+                placeholder="Description of image?"
+                value={props.description}
+                onChange={(event) => {props.setDescription(event.target.value)}}
+            /></label>  
+    
+        </div>
+        <button type="submit">Save</button>
+        </form>
+    )
+}
+
+export default GalleryForm;
